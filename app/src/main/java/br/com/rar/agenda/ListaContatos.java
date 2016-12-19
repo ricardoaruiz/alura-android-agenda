7eed5da91c00eaf19c6547a9897076a61bdd579f
Ricardo Ruiz <ralmendro@ciandt.com>
2016-12-19 09:20:12 -0200

    Criação do Projeto

---
+++ app/src/main/java/br/com/rar/agenda/ListaContatos.java
@@ -0,0 +1,27 @@
+package br.com.rar.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaContatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contatos);
+
+        String[] alunos = {"Ricardo Ruiz", "Guilherme Ruiz", "Cinthya Carvalho", "Aluno1", "Aluno2",
+                           "Aluno3", "Aluno4", "Aluno5", "Aluno6", "Aluno7", "Aluno8"};
+
+        ListView listAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        listAlunos.setAdapter(adapter);
+
+    }
+}
