ef484452db03bcd854eac253393f19d23c03d040
Ricardo Ruiz <ralmendro@ciandt.com>
2016-12-20 15:10:28 -0200

    2. Adicionando Alunos na Agenda - 06. Utilizando Menus

--- app/src/main/java/br/com/rar/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/rar/agenda/FormularioActivity.java
@@ -3,6 +3,9 @@ package br.com.rar.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,15 +16,27 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button btnSalvar = (Button) findViewById(R.id.formulario_btnSalvar);
-        btnSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    //Sobrescreve o menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Adiciona comportamento aos itens do menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok :
                 Toast.makeText(FormularioActivity.this, "Aluno criado com sucesso!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 }
