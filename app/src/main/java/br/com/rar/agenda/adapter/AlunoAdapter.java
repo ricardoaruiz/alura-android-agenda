10519dbb6c80444c514335ed74f85e0358550a5c
Ricardo Ruiz <ralmendro@ciandt.com>
2016-12-30 23:10:12 -0200

    03. Melhorando o visual da lista de alunos - 03. Melhorias no layout

--- app/src/main/java/br/com/rar/agenda/adapter/AlunoAdapter.java
+++ app/src/main/java/br/com/rar/agenda/adapter/AlunoAdapter.java
@@ -1,13 +1,22 @@
 package br.com.rar.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
 import java.util.List;
 
+import br.com.rar.agenda.R;
 import br.com.rar.agenda.modelo.Aluno;
 
 /**
@@ -42,9 +51,25 @@ public class AlunoAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(this.context);
-        textView.setText(this.alunos.get(position).toString());
-        return textView;
+
+        Aluno aluno = this.alunos.get(position);
+
+        View view = convertView != null ? convertView : LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        if(aluno.getFoto() != null && new File(aluno.getFoto()).exists()) {
+            ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+            Bitmap bitmap = BitmapFactory.decodeFile(aluno.getFoto());
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+        }
+
+        return view;
     }
 }
 
