16416b683c61207785d32f209d943817630a4a66
Ricardo Ruiz <ralmendro@ciandt.com>
2017-01-09 15:23:20 -0200

    02. Reaproveitando telas com Fragment - 03. Usando o Fragment

--- app/src/main/java/br/com/rar/agenda/ProvasActivity.java
+++ app/src/main/java/br/com/rar/agenda/ProvasActivity.java
@@ -1,6 +1,8 @@
 package br.com.rar.agenda;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.rar.agenda.fragment.ListaProvasFragment;
 import br.com.rar.agenda.modelo.Prova;
 
 public class ProvasActivity extends AppCompatActivity {
@@ -21,28 +24,10 @@ public class ProvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provas);
 
-        List<String> topicoPort = Arrays.asList("sujeito", "pronome", "verbo");
-        Prova provaPortugues = new Prova("Portugues", "01/02/2017", topicoPort);
-
-        List<String> topicoMat = Arrays.asList("equação de segundo grau", "trigonometria");
-        Prova provaMatematica = new Prova("Matemática", "10/02/2017", topicoMat);
-
-        List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
-
-        ArrayAdapter<Prova> provaAdapter = new ArrayAdapter<Prova>(this, android.R.layout.simple_list_item_1, provas);
-        ListView listaProvas = (ListView) findViewById(R.id.lista_provas);
-        listaProvas.setAdapter(provaAdapter);
-
-        listaProvas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Prova prova = (Prova) parent.getItemAtPosition(position);
-                Intent irParaDetalhesProva = new Intent(ProvasActivity.this, DetalhesProvaActivity.class);
-                irParaDetalhesProva.putExtra("prova", prova);
-                startActivity(irParaDetalhesProva);
-                Toast.makeText(ProvasActivity.this, "Clicou em " + prova.getMateria(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction tx = fragmentManager.beginTransaction();
+        tx.replace(R.id.frame_principal, new ListaProvasFragment());
+        tx.commit();
 
     }
 }
---
+++ app/src/main/java/br/com/rar/agenda/fragment/ListaProvasFragment.java
@@ -0,0 +1,60 @@
+package br.com.rar.agenda.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.rar.agenda.DetalhesProvaActivity;
+import br.com.rar.agenda.ProvasActivity;
+import br.com.rar.agenda.R;
+import br.com.rar.agenda.modelo.Prova;
+
+/**
+ * Created by ralmendro on 1/9/17.
+ */
+
+public class ListaProvasFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_lista_provas, container, false);
+
+        List<String> topicoPort = Arrays.asList("sujeito", "pronome", "verbo");
+        Prova provaPortugues = new Prova("Portugues", "01/02/2017", topicoPort);
+
+        List<String> topicoMat = Arrays.asList("equação de segundo grau", "trigonometria");
+        Prova provaMatematica = new Prova("Matemática", "10/02/2017", topicoMat);
+
+        List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
+
+        ArrayAdapter<Prova> provaAdapter = new ArrayAdapter<Prova>(getContext(), android.R.layout.simple_list_item_1, provas);
+        ListView listaProvas = (ListView) view.findViewById(R.id.lista_provas);
+        listaProvas.setAdapter(provaAdapter);
+
+        listaProvas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Prova prova = (Prova) parent.getItemAtPosition(position);
+                Intent irParaDetalhesProva = new Intent(getContext(), DetalhesProvaActivity.class);
+                irParaDetalhesProva.putExtra("prova", prova);
+                startActivity(irParaDetalhesProva);
+                Toast.makeText(getContext(), "Clicou em " + prova.getMateria(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
